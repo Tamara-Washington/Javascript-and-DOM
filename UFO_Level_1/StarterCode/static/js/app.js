84a13446de7a68a3adadd0afdbc395d0436cbca3
tamara washington <tamey.washington@gmail.com>
2021-03-05 16:45:22 -0600

    updated comments

--- UFO_Level_1/StarterCode/static/js/app.js
+++ UFO_Level_1/StarterCode/static/js/app.js
@@ -1,33 +1,46 @@
 // from data.js
 let tableData = data;
 
-//Get a handle on the table body and button tags
+//Get a handle on the table body 
 let tbody = d3.select('tbody');
+//Get a handle on the button
 let button = d3.select('#filter-btn');
 
-// processRecord(tableInfo)
 button.on('click', handleClick);
 
+
 //Callback function to be use in forEach
-data.forEach(processRecord);
-function processRecord(record) {
-let row = tbody.append('tr');
-Object.values(record).forEach(
-    function (value) {
-        row.append('td').text(value);
-    }
+    data.forEach(function(record){
+    let row = tbody.append('tr');
+    Object.values(record).forEach(
+        function (value) {
+            row.append('td').text(value);
+        }
     );
-}
+})
 
 //Filter the table
 function handleClick() {
-    let date = d3.select('input').property('value');
-    let selectData = tableInfo;
 
-    if (date) {
-        selectData = selectData.filter(row => row.datetime === date);
+    let inputElement = d3.select('#datetime');
+    //Capture date entered
+    let inputValue = inputElement.property('value');
+    console.log(inputValue);
+    //Empty original data
+    d3.select('tbody').html('');
+    var tbody = d3.select ('tbody')
+    if (inputValue) {
+        selectData = tableData.filter(row => row.datetime == inputValue);
     };
+    console.log(selectData);
+    selectData.forEach(processRecord);
+    function processRecord(record) {
+        let row = tbody.append('tr');
+        Object.values(record).forEach(
+            function (value) {
+                row.append('td').text(value);
+            }
+        );
+    }
 
-    d3.select('input').property('value','');
-    processRecord(selectData);
 };
